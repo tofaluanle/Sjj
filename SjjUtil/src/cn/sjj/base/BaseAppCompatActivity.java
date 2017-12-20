cca6e5d8e4bf15593008a4ce20d82f134e05bd78
sjj@home <songjiangjiang@sina.com>
2017-12-20 23:59:16 +0800

    * [util] 更新BaseAppCompatActivity和BaseFragment的代码

--- SjjUtil/src/cn/sjj/base/BaseAppCompatActivity.java
+++ SjjUtil/src/cn/sjj/base/BaseAppCompatActivity.java
@@ -12,12 +12,8 @@ import cn.sjj.Logger;
  */
 public abstract class BaseAppCompatActivity extends AppCompatActivity {
 
-    protected static final boolean DEBUG = IStatics.DEBUG;
+    protected static final boolean DEBUG    = IStatics.DEBUG;
     protected static final boolean UI_DEBUG = IStatics.UI_DEBUG;
-
-    protected abstract void findView();
-
-    protected abstract void registerListener();
 
     protected abstract void init();
 
@@ -85,5 +81,11 @@ public abstract class BaseAppCompatActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         Logger.i(DEBUG, this + " onActivityResult");
+    }
+
+    protected void findView() {
+    }
+
+    protected void registerListener() {
     }
 }
