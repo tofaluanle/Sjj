c20a2d9d02f5e529695e546bbdc582de16bff056
songjj@work <songjj@ziroom.com>
2019-04-04 10:13:58 +0800

    + [util] 增加模板类

---
+++ SjjUtil/src/cn/sjj/MainActivity.java
@@ -0,0 +1,115 @@
+package cn.sjj;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //region 1
+    public void click1(View v) {
+    }
+    //endregion
+
+    //region 2
+    public void click2(View v) {
+    }
+
+    //endregion
+
+    //region 3
+    public void click3(View v) {
+    }
+    //endregion
+
+    //region 4
+    public void click4(View v) {
+    }
+    //endregion
+
+    //region 5
+    public void click5(View v) {
+    }
+    //endregion
+
+    //region 6
+    public void click6(View v) {
+    }
+    //endregion
+
+    //region 7
+    public void click7(View v) {
+    }
+    //endregion
+
+    //region 8
+    public void click8(View v) {
+    }
+    //endregion
+
+    //region 9
+    public void click9(View v) {
+    }
+    //endregion
+
+    //region 10
+    public void click10(View v) {
+    }
+    //endregion
+
+    //region 11
+    public void click11(View v) {
+    }
+    //endregion
+
+    //region 12
+    public void click12(View v) {
+    }
+    //endregion
+
+    //region 13
+    public void click13(View v) {
+    }
+    //endregion
+
+    //region 14
+    public void click14(View v) {
+    }
+    //endregion
+
+    //region 15
+    public void click15(View v) {
+    }
+    //endregion
+
+    //region 16
+    public void click16(View v) {
+    }
+    //endregion
+
+    //region 17
+    public void click17(View v) {
+    }
+    //endregion
+
+    //region 18
+    public void click18(View v) {
+    }
+    //endregion
+
+    //region 19
+    public void click19(View v) {
+    }
+    //endregion
+
+    //region 20
+    public void click20(View v) {
+    }
+    //endregion
+}
