fe1af669edfc4d1cc794ecf0cd538c64dacd0704
songjj@work <songjj@ziroom.com>
2019-07-17 15:47:47 +0800

    * [util] 优化BaseBean的NotToString逻辑

--- SjjUtil/src/cn/sjj/bean/BaseBean.java
+++ SjjUtil/src/cn/sjj/bean/BaseBean.java
@@ -1,5 +1,7 @@
 package cn.sjj.bean;
 
+import android.os.Parcelable;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 
@@ -14,42 +16,49 @@ import cn.sjj.annotation.NotToString;
  */
 public class BaseBean {
 
-	@Override
-	public String toString() {
-		if (!BuildConfig.DEBUG) {
-			return super.toString();
-		}
+    @Override
+    public String toString() {
+        if (!BuildConfig.DEBUG) {
+            return super.toString();
+        }
 
-		StringBuilder sb = new StringBuilder();
-		sb.append(getClass().getSimpleName() + " [ ");
-		getValue(getClass(), sb);
-		sb.delete(sb.length() - 2, sb.length());
-		sb.append(" ]");
-		return sb.toString();
-	}
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName() + " [ ");
+        getValue(getClass(), sb);
+        sb.delete(sb.length() - 2, sb.length());
+        sb.append(" ]");
+        return sb.toString();
+    }
 
-	private void getValue(Class clazz, StringBuilder sb) {
-		Field[] fields = clazz.getDeclaredFields();
-		for (Field field : fields) {
-			try {
-				if (Modifier.isFinal(field.getModifiers()) || field.isAnnotationPresent(NotToString.class)) {
-					continue;
-				}
-				field.setAccessible(true);
-				Object object = field.get(this);
-				String value = field.getName() + " = " + object + ", ";
-				sb.append(value);
-			} catch (IllegalArgumentException e) {
-				e.printStackTrace();
-			} catch (IllegalAccessException e) {
-				e.printStackTrace();
-			}
-		}
-		String name = BaseBean.class.getName();
-		if (clazz.getName().equals(name)) {
-			return;
-		}
-		getValue(clazz.getSuperclass(), sb);
-	}
+    private void getValue(Class clazz, StringBuilder sb) {
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field : fields) {
+            try {
+                if (isNotToStringField(field)) {
+                    continue;
+                }
+
+                field.setAccessible(true);
+                Object object = field.get(this);
+                String value = field.getName() + " = " + object + ", ";
+                sb.append(value);
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        String name = BaseBean.class.getName();
+        if (clazz.getName().equals(name)) {
+            return;
+        }
+        getValue(clazz.getSuperclass(), sb);
+    }
+
+    protected boolean isNotToStringField(Field field) {
+        return Modifier.isFinal(field.getModifiers())
+                || field.isAnnotationPresent(NotToString.class)
+                || Parcelable.Creator.class.isAssignableFrom(field.getType());
+    }
 
 }
