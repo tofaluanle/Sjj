2eb7954489b4c69a375d49f1f335c37d15234f82
songjj@work <songjj@ziroom.com>
2019-09-09 15:32:34 +0800

    + [util] 引入kotlin

---
+++ SjjUtil/src/cn/sjj/MainActivity_KT.kt
@@ -0,0 +1,93 @@
+package cn.sjj
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+
+class MainActivity_KT : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    //region 1
+    fun click1(v: View) {}
+    //endregion
+
+    //region 2
+    fun click2(v: View) {}
+    //endregion
+
+    //region 3
+    fun click3(v: View) {}
+    //endregion
+
+    //region 4
+    fun click4(v: View) {}
+    //endregion
+
+    //region 5
+    fun click5(v: View) {}
+    //endregion
+
+    //region 6
+    fun click6(v: View) {}
+    //endregion
+
+    //region 7
+    fun click7(v: View) {}
+    //endregion
+
+    //region 8
+    fun click8(v: View) {}
+    //endregion
+
+    //region 9
+    fun click9(v: View) {}
+    //endregion
+
+    //region 10
+    fun click10(v: View) {}
+    //endregion
+
+    //region 11
+    fun click11(v: View) {}
+    //endregion
+
+    //region 12
+    fun click12(v: View) {}
+    //endregion
+
+    //region 13
+    fun click13(v: View) {}
+    //endregion
+
+    //region 14
+    fun click14(v: View) {}
+    //endregion
+
+    //region 15
+    fun click15(v: View) {}
+    //endregion
+
+    //region 16
+    fun click16(v: View) {}
+    //endregion
+
+    //region 17
+    fun click17(v: View) {}
+    //endregion
+
+    //region 18
+    fun click18(v: View) {}
+    //endregion
+
+    //region 19
+    fun click19(v: View) {}
+    //endregion
+
+    //region 20
+    fun click20(v: View) {}
+    //endregion
+}
