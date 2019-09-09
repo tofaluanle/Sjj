8caf39238d916b170d63befc7a2ee3e77ee8bdfc
songjj@work <songjj@ziroom.com>
2019-09-09 15:34:23 +0800

    * [util] 格式化

--- SjjUtil/src/cn/sjj/MainActivity_KT.kt
+++ SjjUtil/src/cn/sjj/MainActivity_KT.kt
@@ -12,82 +12,122 @@ class MainActivity_KT : AppCompatActivity() {
     }
 
     //region 1
-    fun click1(v: View) {}
+    fun click1(v: View) {
+
+    }
     //endregion
 
     //region 2
-    fun click2(v: View) {}
+    fun click2(v: View) {
+
+    }
     //endregion
 
     //region 3
-    fun click3(v: View) {}
+    fun click3(v: View) {
+
+    }
     //endregion
 
     //region 4
-    fun click4(v: View) {}
+    fun click4(v: View) {
+
+    }
     //endregion
 
     //region 5
-    fun click5(v: View) {}
+    fun click5(v: View) {
+
+    }
     //endregion
 
     //region 6
-    fun click6(v: View) {}
+    fun click6(v: View) {
+
+    }
     //endregion
 
     //region 7
-    fun click7(v: View) {}
+    fun click7(v: View) {
+
+    }
     //endregion
 
     //region 8
-    fun click8(v: View) {}
+    fun click8(v: View) {
+
+    }
     //endregion
 
     //region 9
-    fun click9(v: View) {}
+    fun click9(v: View) {
+
+    }
     //endregion
 
     //region 10
-    fun click10(v: View) {}
+    fun click10(v: View) {
+
+    }
     //endregion
 
     //region 11
-    fun click11(v: View) {}
+    fun click11(v: View) {
+
+    }
     //endregion
 
     //region 12
-    fun click12(v: View) {}
+    fun click12(v: View) {
+
+    }
     //endregion
 
     //region 13
-    fun click13(v: View) {}
+    fun click13(v: View) {
+
+    }
     //endregion
 
     //region 14
-    fun click14(v: View) {}
+    fun click14(v: View) {
+
+    }
     //endregion
 
     //region 15
-    fun click15(v: View) {}
+    fun click15(v: View) {
+
+    }
     //endregion
 
     //region 16
-    fun click16(v: View) {}
+    fun click16(v: View) {
+
+    }
     //endregion
 
     //region 17
-    fun click17(v: View) {}
+    fun click17(v: View) {
+
+    }
     //endregion
 
     //region 18
-    fun click18(v: View) {}
+    fun click18(v: View) {
+
+    }
     //endregion
 
     //region 19
-    fun click19(v: View) {}
+    fun click19(v: View) {
+
+    }
     //endregion
 
     //region 20
-    fun click20(v: View) {}
+    fun click20(v: View) {
+
+    }
     //endregion
 }
